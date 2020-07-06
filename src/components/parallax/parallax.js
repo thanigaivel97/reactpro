61512110f157411f723d136f39920a1c1c198725
thanigaivel97 <thanigaivelj59@gmail.com>
2020-07-06 20:14:25 +0530

    first commit

--- src/components/parallax/parallax.js
+++ src/components/parallax/parallax.js
@@ -1,92 +1,54 @@
 import React, { Component } from 'react'
-import '../parallax/parallax.css'
-import axios from  'axios'
+import axios from 'axios';
 
 export default class parallax extends Component {
     constructor(props){
         super(props);
         this.state = {
-            firstname : "",
-            lastname : "",
-            emailid : "",
-            phonenumber : ""
+            arr : [] 
         }
-        this.onchangehandler = this.onchangehandler.bind(this);
-        this.onsubmithandler = this.onsubmithandler.bind(this);
+      
     }
 
-    onsubmithandler(event){
-        event.preventDefault();
-        
-        const data = new FormData(event.target);
-        let emailid = data.get('email');
-        let firstname = data.get('firstname');
-        let lastname = data.get('lastname');
-        let phonenumber = data.get('phonenumber');
-        const po = {
-            emailid :emailid,
-            firstname : firstname,
-            lastname :lastname,
-            phonenumber : phonenumber
-        }
-        console.log(emailid,firstname);
-       
-        fetch('http://localhost:8080/auth/signup',{
-            method : 'POST',
-            body : JSON.stringify(po),
-            headers : {
-                'Content-Type' : 'application/json'
-            }
-        }).then(res => {
-            return res.json();
-        }).then(da=>{
-            console.log(da);
-        })
-        
-        
-        
-    }
+    componentDidMount(){
+        axios.get('http://localhost:8080/time/values').then(data => {
+            console.log(data.data.data);
+            const arr  = data.data.data.map(ite => {
+                
+                let dataobj = new Date(ite.timestamp * 1000);
+                let ustime = dataobj.toLocaleString("en-US", { 
+                    timeZone: "America/New_York"  
+                }); 
+                let lontime = dataobj.toLocaleString("en-US", { 
+                    timeZone: "Europe/London"  
+                }); 
 
-    onchangehandler(e){
-        console.log(e.target.name, e.target.value);
-        let h = e.target.name;
-        
-        
+                return {
+                    assian : dataobj,
+                    newyork : ustime,
+                    lontime : lontime
+                }
+            })
+            this.setState({arr : arr} ,() => {
+                console.log(arr)
 
-    }
-
-
-    render() {
-        return (
-        <div>
-            <form onSubmit={this.onsubmithandler} style={{border:"1px solid #ccc"}}>
-  <div className="container">
-    <h1>Sign Up</h1>
-    <p>Please fill in this form to create an account.</p>
-    <hr/>
-
-    <label for="email"><b>Firstname</b></label>
-    <input type="text" onChange={this.onchangehandler} placeholder="Enter Firstname" name="firstname" required/>
-
-    <label for="email"><b>LastName</b></label>
-    <input type="text" onChange={this.onchangehandler} placeholder="Enter Lastname" name="lastname" required/>
-
-    <label for="email"><b>Email</b></label>
-    <input type="text" onChange={this.onchangehandler}  placeholder="Enter Email" name="email" required/>
-
-    <label for="email"><b>Phonenumber</b></label>
-    <input type="text" onChange={this.onchangehandler} placeholder="Enter Phonenumber" name="phonenumber" required/>
-    
-
-    <div className="clearfix">
-      <button type="button" className="cancelbtn">Cancel</button>
-      <button type="submit" className="signupbtn">Sign Up</button>
-    </div>
-  </div>
-</form>
-             
-        </div>
+            } )
             
+        })
+
+    }
+
+   
+    render() {
+        
+        
+
+        return (
+            <div>
+                {this.state.arr.map(ite => {
+            return `assian : ${ite.assian} newyork : ${ite.newyork} london : ${ite.lontime} ++++++++++++++++++++++++`
+        })}
+            </div>
         )
     }
 }
